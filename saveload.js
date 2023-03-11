5e8cab1e59a81223c38367ce5cbf63cba67041cc
stellarzenith <18901928+stellarzenith@users.noreply.github.com>
2023-03-11 18:14:16 +0000

    Import/Export

---
+++ saveload.js
@@ -0,0 +1,46 @@
+function exportPlate(){
+    const adventurerplate = {
+        title: document.getElementById("title").value,
+        titlecolor: document.getElementById("titlecolor").value,
+        name: document.getElementById("name").value,
+        namecolor: document.getElementById("namecolor").value,
+        world: document.getElementById("world").value,
+        dc: document.getElementById("dc").value,
+        servercolor: document.getElementById("servercolor").value,
+        serverglow: document.getElementById("serverglow").value,
+        classjob: document.getElementById("classjob").value,
+        classjobcolor: document.getElementById("classjobcolor").value,
+        level: document.getElementById("level").value,
+        gc: document.getElementById("gc").value,
+        gccolor: document.getElementById("gccolor").value,
+        charinfo: document.getElementById("charinfo").value,
+        charinfocolor: document.getElementById("charinfocolor").value,
+        headingcolor: document.getElementById("headingcolor").value,
+        platetopcolor: document.getElementById("platetopcolor").value,
+        platebtmcolor: document.getElementById("platebtmcolor").value,
+        portrait: document.getElementById("portrait").value,
+        alttext: document.getElementById("alttext").value 
+    }
+    
+    var a = document.createElement("a");
+    a.href = window.URL.createObjectURL(new Blob([JSON.stringify(adventurerplate)], {type: "text/plain"}));
+    if(!!adventurerplate.name){
+        a.download = adventurerplate.name + ".json";
+    }
+    else{
+        a.download = "adventurer-plate.json";
+    }
+    a.click(); 
+}
+
+function importPlate(files){
+    var fr=new FileReader();
+    fr.readAsText(files[0]);
+    fr.onload = function(){
+        const adventurerplate = JSON.parse(fr.result);
+        for(key in adventurerplate){
+            document.getElementById(key).value = adventurerplate[key];
+        }
+        generateCSS();
+    }
+}
